17cbe38b910c8a51dd34afacb5e720937342ab5e
krquinton <ton.quinton@gmail.com>
2021-09-16 21:04:32 +0800

    initial commit
    initial commit

---
+++ data.js
@@ -0,0 +1,415 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-banlic-1",
+      "name": "Banlic 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.31965026234708027,
+        "pitch": 0.2856760351444514,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10128541718677475,
+          "pitch": 0.3268696944067564,
+          "rotation": 0,
+          "target": "1-banlic-2"
+        },
+        {
+          "yaw": -0.2600970587148268,
+          "pitch": 0.1766863593677197,
+          "rotation": 0,
+          "target": "2-banlic-3"
+        },
+        {
+          "yaw": -0.11620099810748563,
+          "pitch": 0.11198185599264221,
+          "rotation": 0,
+          "target": "3-banlic-4"
+        },
+        {
+          "yaw": -0.05884806881866389,
+          "pitch": -0.004322537763965073,
+          "rotation": 0,
+          "target": "5-banlic-6"
+        },
+        {
+          "yaw": 2.0356896344611517,
+          "pitch": 0.4111668687687029,
+          "rotation": 0,
+          "target": "6-bridge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-banlic-2",
+      "name": "Banlic 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.40545035884789904,
+        "pitch": 0.5146342456306314,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3873580931372622,
+          "pitch": 0.09119506889500784,
+          "rotation": 0,
+          "target": "2-banlic-3"
+        },
+        {
+          "yaw": -0.22156229120524884,
+          "pitch": 0.048721158068561365,
+          "rotation": 0,
+          "target": "3-banlic-4"
+        },
+        {
+          "yaw": -0.07996520068972757,
+          "pitch": -0.042284158235215585,
+          "rotation": 0,
+          "target": "5-banlic-6"
+        },
+        {
+          "yaw": 3.0397111206043306,
+          "pitch": -0.14920721904629097,
+          "rotation": 0,
+          "target": "0-banlic-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-banlic-3",
+      "name": "Banlic 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8416120631104143,
+          "pitch": 0.1250023933579314,
+          "rotation": 0,
+          "target": "1-banlic-2"
+        },
+        {
+          "yaw": 2.8170787139882734,
+          "pitch": -0.017431949447757944,
+          "rotation": 0,
+          "target": "0-banlic-1"
+        },
+        {
+          "yaw": -0.09444706502648792,
+          "pitch": 0.10814222932330786,
+          "rotation": 0,
+          "target": "3-banlic-4"
+        },
+        {
+          "yaw": -0.010126304736399305,
+          "pitch": 0.029009466778227022,
+          "rotation": 0,
+          "target": "4-banlic-5"
+        },
+        {
+          "yaw": -0.012753140918420058,
+          "pitch": -0.07268520316796945,
+          "rotation": 0,
+          "target": "5-banlic-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-banlic-4",
+      "name": "Banlic 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.845741727488285,
+        "pitch": 0.5718039566698891,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.115744580867064,
+          "pitch": 0.21346732044562167,
+          "rotation": 0,
+          "target": "4-banlic-5"
+        },
+        {
+          "yaw": -3.0247122408671974,
+          "pitch": 0.049821993673139886,
+          "rotation": 0,
+          "target": "5-banlic-6"
+        },
+        {
+          "yaw": -0.09095275014690252,
+          "pitch": 0.20386297272836096,
+          "rotation": 0,
+          "target": "2-banlic-3"
+        },
+        {
+          "yaw": -0.006620623107599144,
+          "pitch": 0.06700167559962367,
+          "rotation": 0,
+          "target": "1-banlic-2"
+        },
+        {
+          "yaw": 0.09938983781521316,
+          "pitch": 0.00346568345806908,
+          "rotation": 0,
+          "target": "0-banlic-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-banlic-5",
+      "name": "Banlic 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.057904508709804,
+        "pitch": 0.4922816529278613,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0923584908007706,
+          "pitch": 0.06781044172706885,
+          "rotation": 0,
+          "target": "5-banlic-6"
+        },
+        {
+          "yaw": 0.013064878324460238,
+          "pitch": 0.11056148536117405,
+          "rotation": 0,
+          "target": "2-banlic-3"
+        },
+        {
+          "yaw": -0.10062820981734077,
+          "pitch": 0.03722881188354954,
+          "rotation": 0,
+          "target": "1-banlic-2"
+        },
+        {
+          "yaw": -0.07233352156948314,
+          "pitch": -0.07267822832673332,
+          "rotation": 0,
+          "target": "0-banlic-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-banlic-6",
+      "name": "Banlic 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024326060493798707,
+          "pitch": 0.1637770624217989,
+          "rotation": 0,
+          "target": "4-banlic-5"
+        },
+        {
+          "yaw": -0.030083894627237484,
+          "pitch": 0.05681123845870317,
+          "rotation": 0,
+          "target": "3-banlic-4"
+        },
+        {
+          "yaw": -0.06907189246652834,
+          "pitch": -0.06677559378440279,
+          "rotation": 0,
+          "target": "0-banlic-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bridge",
+      "name": "Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4713203523737093,
+        "pitch": 0.5421402163303277,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3183608055982816,
+          "pitch": -0.0586342815430001,
+          "rotation": 0,
+          "target": "0-banlic-1"
+        },
+        {
+          "yaw": 3.065407986438501,
+          "pitch": 0.20550940783942906,
+          "rotation": 0,
+          "target": "1-banlic-2"
+        },
+        {
+          "yaw": -3.006511285679178,
+          "pitch": 0.13318491811320854,
+          "rotation": 0,
+          "target": "2-banlic-3"
+        },
+        {
+          "yaw": -2.8188804722139373,
+          "pitch": 0.029395476668241827,
+          "rotation": 0,
+          "target": "5-banlic-6"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "NSRP - Banlic Depot Virtual Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
